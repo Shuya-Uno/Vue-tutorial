7592398035c86cbdd87453c39168e2765f05fe98
Shuya <shuya.uno@gmail.com>
2023-08-28 01:30:38 +0900

    Conditional Rendering

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data(){
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
@@ -8,9 +9,8 @@ const app = Vue.createApp({
     },
 
     methods: {
-        changeTitle(title){
-            // this.title = 'Words of Radiance'
-            this.title = title
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 
