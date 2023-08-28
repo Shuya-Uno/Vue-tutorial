38fc213a66be27c79c82a1b669aae3c200633e73
Shuya <shuya.uno@gmail.com>
2023-08-29 00:27:37 +0900

    Attribute Binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data(){
         return {
+            url: 'https://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                {title: 'Name of the Wind', author: 'Patrick Rothfuss'},
-                {title: 'The Way of Kings', author: 'Brandon Sanderson'},
-                {title: 'The Final Empire', author: 'Brandon Sanderson'}
+                {title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg'},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg'},
+                {title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'}
             ]
         }
     },
