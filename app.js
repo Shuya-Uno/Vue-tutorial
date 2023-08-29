8cffd18da21a00431af4a67508fc1ff288acacd5
Shuya <shuya.uno@gmail.com>
2023-08-29 23:31:45 +0900

    Dynamic Classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'https://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                {title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg'},
-                {title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg'},
-                {title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'}
+                {title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg', isfav: true},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isfav: false},
+                {title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isfav: true}
             ]
         }
     },
