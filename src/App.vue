caf03ac912baed88ec31fc8467d74af6fdb1efdf
Shuya <shuya.uno@gmail.com>
2023-08-30 12:03:27 +0900

    Modal-project Init (The Vue CLI & Bigger Projects)

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<!--
+  Gets rendered to the "app" div
+   (not directly coding inside the div, in order to utilize the concept of multiple components).
+-->
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'My First Vue App :)'
+    }
+  }
+}
+/*
+  All dynamic development methods that was used in widget-size project
+   can also be used in SPA-size projects (Such as the data function above).
+*/
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+/* 
+  Every style written inside here is placed into the <head>,
+   being a global style for any components by default.
+*/
+</style>
