021a4d261e147439b4f35b78a25d640d6f98cd79
Shuya <shuya.uno@gmail.com>
2023-08-30 21:42:42 +0900

    Scoped & Global CSS

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal Title</h1>
             <p>modal content</p>
         </div>
     </div>
@@ -22,4 +23,21 @@
         width: 100%;
         height: 100%;
     }
+
+    .modal h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+
+    .modal p {
+        font-style: normal;
+    }
+
+    /* 
+        Adding "scoped" attribute is one way to scope styles to a component,
+         but it forces a little computing labor to the computer, since
+          it injects an random attribute to every HTML tag that is involved,
+           to identify target elements
+    */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css'
+/*
+    If you want to make a file take effect globally in all components,
+     you should import it inside main.js as above
+*/
 
 createApp(App).mount('#app')
