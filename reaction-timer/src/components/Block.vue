080ac5040805b6715a9632da0f9dbbaf610e7995
Shuya <shuya.uno@gmail.com>
2023-09-13 02:28:28 +0900

    Creating a Timer

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
         click me
     </div>
 </template>
@@ -10,22 +10,29 @@
 
         data(){
             return {
-                showBlock: false
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
+            }
+        },
+        methods: {
+            startTimer(){
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            },
+            stopTimer(){
+                clearInterval(this.timer)
+                console.log(this.reactionTime)
             }
         },
 
         mounted(){
             setTimeout(() => {
                 this.showBlock = true
-                console.log(this.delay)
+                this.startTimer()
             }, this.delay);
             console.log('component mounted')
-        },
-        updated(){
-            console.log('component updated')
-        },
-        unmounted(){
-            console.log('unmounted')
         }
         /* 
             lifecycle hooks → function triggers which each corresponds to
