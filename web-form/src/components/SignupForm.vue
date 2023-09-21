78214e281fc09bc6be8637c19b5dd9199ffcd4c5
Shuya <shuya.uno@gmail.com>
2023-09-21 23:38:54 +0900

    v-model with Keyboard Events

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -12,22 +12,16 @@
             <option value="designer">Web Designer</option>
         </select>
 
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+
+        <div v-for="skill in skills" :key="skill" class="pill">
+            {{ skill }}
+        </div>
+
         <div class="terms">
             <input type="checkbox" v-model="terms" required />
             <label>Accept terms and conditions</label>
-        </div>
-
-        <div>
-            <input type="checkbox" value="shaun" v-model="names" />
-            <label>Shaun</label>
-        </div>
-        <div>
-            <input type="checkbox" value="yoshi" v-model="names" />
-            <label>Yoshi</label>
-        </div>
-        <div>
-            <input type="checkbox" value="mario" v-model="names" />
-            <label>Mario</label>
         </div>
     </form>
 
@@ -35,7 +29,6 @@
     <p>Password: {{ password }}</p>
     <p>Role: {{ role }}</p>
     <p>Terms Accepted: {{ terms }}</p>
-    <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -46,7 +39,18 @@
                 password: '',
                 role: 'designer',
                 terms: false,
-                names: []
+                tempSkill: '',
+                skills: []
+            }
+        },
+        methods: {
+            addSkill(e){
+                if (e.key === ',' && this.tempSkill){
+                    if (!this.skills.includes(this.tempSkill)){
+                        this.skills.push(this.tempSkill)
+                    }
+                    this.tempSkill = ''
+                } 
             }
         }
     }
