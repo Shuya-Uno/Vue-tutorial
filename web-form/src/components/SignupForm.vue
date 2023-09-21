58501d234b331305c4595b8d8906bb877275b0bf
Shuya <shuya.uno@gmail.com>
2023-09-21 22:35:03 +0900

    Checkboxes (v-model)

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -11,11 +11,31 @@
             <option value="developer">Web Developer</option>
             <option value="designer">Web Designer</option>
         </select>
+
+        <div class="terms">
+            <input type="checkbox" v-model="terms" required />
+            <label>Accept terms and conditions</label>
+        </div>
+
+        <div>
+            <input type="checkbox" value="shaun" v-model="names" />
+            <label>Shaun</label>
+        </div>
+        <div>
+            <input type="checkbox" value="yoshi" v-model="names" />
+            <label>Yoshi</label>
+        </div>
+        <div>
+            <input type="checkbox" value="mario" v-model="names" />
+            <label>Mario</label>
+        </div>
     </form>
 
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p>
     <p>Role: {{ role }}</p>
+    <p>Terms Accepted: {{ terms }}</p>
+    <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -24,7 +44,9 @@
             return {
                 email: '',
                 password: '',
-                role: 'designer'
+                role: 'designer',
+                terms: false,
+                names: []
             }
         }
     }
@@ -59,5 +81,13 @@
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
+    }
+
+    input[type="checkbox"] {
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
     }
 </style>
