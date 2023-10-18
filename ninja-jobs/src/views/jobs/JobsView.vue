8633c150724d1e47977be4141a60e4b8d54260e0
Shuya <shuya.uno@gmail.com>
2023-10-18 21:27:26 +0900

    Dynamic Links

--- ninja-jobs/src/views/jobs/JobsView.vue
+++ ninja-jobs/src/views/jobs/JobsView.vue
@@ -1,8 +1,18 @@
 <template>
     <h1>Jobs</h1>
-    <template v-for="job in jobs">
-        <h2>{{ job.title }}</h2>
+    <div class="job">
+        <template v-for="job in jobs">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+            <h2>{{ job.title }}</h2>
+        </router-link>
+
+        <!-- 
+            params: { key: value }
+                key → name of the route paramter (in path)
+                value → the value to be inserted into route parameter
+         -->
     </template>
+    </div>
 </template>
 
 <script>
@@ -23,3 +33,23 @@
     In bigger projects, when you have related Views, you can organize them, placing them in the same subfolder.
     The same goes for Components (none page-specific, reusable portions of pages).
  -->
+
+<style>
+    .job h2 {
+        background-color: #f4f4f4;
+        padding: 20px;
+        border-radius: 10px;
+        margin: 10px auto;
+        max-width: 600px;
+        cursor: pointer;
+        color: #444;
+    }
+
+    .job h2:hover {
+        background-color: #ddd;
+    }
+
+    .job a {
+        text-decoration: none;
+    }
+</style>
