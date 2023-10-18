4abac1f982bbc433bf15ddd5ee4f40d64b117d99
Shuya <shuya.uno@gmail.com>
2023-10-18 20:54:22 +0900

    Route Parameters

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import JobsView from '../views/jobs/JobsView.vue'
+import JobDetailsView from '../views/jobs/JobDetailsView.vue'
 
 
 const routes = [
@@ -19,8 +20,15 @@ const routes = [
     path: '/jobs',
     name: 'Jobs',
     component: JobsView
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetailsView
   }
 ]
+
+// to set route for route parameters, you need to add as an indivudal route.
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ ninja-jobs/src/views/jobs/JobDetailsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Job Details</h1>
+        <p>The job id is {{ id }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                id: this.$route.params.id
+            }
+        }
+    }
+
+    /*
+        route parameters can be accessed as a property of a router object, as shown above.
+        the property after "params" will be the word after the ":", of the route path
+         (in this case, "id").    
+    */
+</script>
+
+<style>
+
+</style>
